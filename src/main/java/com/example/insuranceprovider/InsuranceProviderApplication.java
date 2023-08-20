ccb521156a96c7d9a680b8dd1df82b1905a2b536
Solomon Sunu <solomonsunu@gmail.com>
2023-08-20 10:15:36 +0000

    initial commit

---
+++ src/main/java/com/example/insuranceprovider/InsuranceProviderApplication.java
@@ -0,0 +1,30 @@
+package com.example.insuranceprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/insurance-service")
+public class InsuranceProviderApplication {
+
+  @GetMapping("getUpdatePlans")
+  public List<String> getNewPlans(){
+    return Stream.of("New Premium","New Gold","Platinum").collect(Collectors.toList());
+  }
+  @GetMapping("getPlans")
+  public List<String> getPlans(){
+    return Stream.of("Premium","Gold","Platinum").collect(Collectors.toList());
+  }
+  public static void main(String[] args) {
+    SpringApplication.run(InsuranceProviderApplication.class, args);
+  }
+
+}
